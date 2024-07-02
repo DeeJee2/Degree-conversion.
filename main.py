5ccdaf1d655db209337ca755f8ae862a6fb2d790
DeeJee <zedxi236@gmail.com>
2024-07-02 22:46:14 +0300

    main

---
+++ main.py
@@ -0,0 +1,14 @@
+print("1.Celsius to Fahrenheit\n2.Fahrenheit to Celsius\n3.Exit")
+choice = int(input("Enter your choice:"))
+if choice == 1:
+    celsius = float(input("Enter temperature in Celsius: "))
+    fahrenheit = (celsius * 9/5) + 32
+    print(f"{celsius} Celsius = {fahrenheit} Fahrenheit")
+elif choice == 2:
+    fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+    celsius = 5/9 * (fahrenheit - 32)
+    print(f"{fahrenheit} Fahrenheit = {celsius} Celsius")
+elif choice == 3:
+    print("Exiting")
+else:
+    print("Invalid choice!")
